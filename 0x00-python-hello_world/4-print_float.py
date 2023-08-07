424bec1e807a1303922f69f8aaf482d9c47db3b3
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-08-06 23:05:43 -0700

    Print float

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# The shebang line above specifies the interpreter to be used for executing this script, in this case, Python 3.
+
+# Assign a floating-point number to the variable 'number'
+number = 3.14159
+
+#Print the value of 'number' with two decimal places using an f-string
+print(f"Float: {number:.2f}")
+
