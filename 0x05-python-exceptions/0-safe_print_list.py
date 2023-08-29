bbf4df9b2bc000bc9aafa1ebbb46269c37d0ce48
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-08-29 08:41:50 +0000

    Task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
-numsum = 0
+total = 0
 for i in range(x):
 try:
-print(f"{my_list[i]}", end=""
-numsum += 1
+print("{}".format(my_list[i]), end="")
+total += 1
 except IndexError:
 break
-print()
-return(numsum)
+print("")
+return (total)
