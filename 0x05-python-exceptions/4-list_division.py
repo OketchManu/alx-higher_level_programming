dfbc2d2efb03f5718ac1cc48dd9c4db2a5f937f4
OketchManu <oketchemmanuelodiwuor@gmail.com>
2023-10-25 10:51:43 -0700

    Mandatory

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+'''
+    A function that divides element by element 2 lists.
+'''
 
 def list_division(my_list_1, my_list_2, list_length):
-tmp = []
-for i in range(list_length):
-div = 0
-try:
-div = my_list_1[i] / my_list_2[i]
-except (ZeroDivisionError, ValueError):
-print("division by 0")
-except (TypeError):
-print("wrong type")
-except (IndexError):
-print("out of range")
-finally:
-tmp.append(div)
-return tmp
+    div_result = []
+    temp_holder = 0
+    for i in range(0, list_length):
+        try:
+            temp_holder = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            temp_holder = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            temp_holder = 0
+            print("division by 0")
+        except IndexError:
+            temp_holder = 0
+            print("out of range")
+        finally:
+            pass
+        div_result.append(temp_holder)
+    return div_result
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+'''
+    A function that raises a name exception with a message.
+'''
+
 def raise_exception_msg(message=""):
-raise NameError(message)
+    raise NameError(message)
